d3a72b12b4d84a995008e7795323c249d65db970
kevin <kevinalvapearson777@gmail.com>
2020-09-11 23:51:00 -0500

    Creando el listado de tareas

--- src/components/proyectos/Proyectos.js
+++ src/components/proyectos/Proyectos.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Sidebar from '../layout/Sidebar';
 import Barra from '../layout/Barra';
 import Formtarea from '../tareas/FormTarea';
+import ListadoTreas from '../tareas/ListadoTareas';
 
 const Proyectos = () => {
     return ( 
@@ -12,7 +13,7 @@ const Proyectos = () => {
                 <main>
                     <Formtarea />
                     <div className="contenedor-tareas">
-                        
+                        <ListadoTreas />
                     </div>
                 </main>
             </div>
