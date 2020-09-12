bd1246f4a045def3a158484ffaeffe34d587c816
kevin <kevinalvapearson777@gmail.com>
2020-09-11 23:32:10 -0500

    creando el Header

---
+++ src/components/proyectos/Proyecto.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Proyecto = ({proyecto}) => {
+
+    return ( 
+        <li>
+            <button
+                type="button"
+                className="btn btn-block"
+            >
+                {proyecto.nombre}
+            </button>
+        </li>
+     );
+}
+ 
+export default Proyecto;
