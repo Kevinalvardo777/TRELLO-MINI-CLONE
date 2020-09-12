ed3dc6bf6679c4f1aff686f6cdf45be356fa1a56
kevin <kevinalvapearson777@gmail.com>
2020-09-12 00:03:42 -0500

    trabajando con la interfaz de tareas

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -13,7 +13,7 @@ const ListadoTareas = () => {
     return ( 
         <Fragment>
             <h2>Proyecto: Tienda Virtual</h2>
-            <ul className="Listadp-tareas">
+            <ul className="listado-tareas">
                 {tareasProyecto.length === 0 
                     ? ( <li className="tarea"><p>No hay tareas</p></li>)
                     : tareasProyecto.map( tarea => (
@@ -23,6 +23,12 @@ const ListadoTareas = () => {
                     ))
                 }
             </ul>
+            <button 
+                type="button"
+                className="btn btn-eliminar"
+            >
+                Eliminar Proyecto &times;
+            </button>
         </Fragment>
         
      );
