408cca473a68bc62e8084b8d216aa297dc63b9a4
kevin <kevinalvapearson777@gmail.com>
2020-09-12 14:22:25 -0500

    mostrar el formulario de nuevos proyectos de forma condicional

--- src/components/proyectos/NuevoProyecto.js
+++ src/components/proyectos/NuevoProyecto.js
@@ -1,6 +1,11 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useState, useContext} from 'react';
+import proyectoContext from '../../context/proyectos/proyectoContext';
 
 const NuevoProyecto = () => {
+
+    // Obtener el state del formulario
+    const proyectosContext = useContext(proyectoContext);
+    const {formulario} = proyectosContext;
 
     // Definir state para proyecto
     const [proyecto, guardarProyecto] = useState({
@@ -35,21 +40,27 @@ const NuevoProyecto = () => {
                 type="button"
                 className="btn btn-block btn-primario"
             >Nuevo Proyecto</button>
-            <form
-                className="formulario-nuevo-proyecto"
-                onSubmit={onSubmitProyecto}
-            >
-                <input 
-                    type="text"
-                    className="input-text"
-                    placeholder="Nombre Proyecto"
-                    name="nombre"
-                    value={nombre}
-                    onChange={onChangeProyecto}
-                />
+            {formulario 
+            ?
+            (
+                <form
+                    className="formulario-nuevo-proyecto"
+                    onSubmit={onSubmitProyecto}
+                >
+                    <input 
+                        type="text"
+                        className="input-text"
+                        placeholder="Nombre Proyecto"
+                        name="nombre"
+                        value={nombre}
+                        onChange={onChangeProyecto}
+                    />
 
-                <input type="submit" className="btn btn-primario" value="Agregar Proyecto"/>
-            </form>
+                    <input type="submit" className="btn btn-primario" value="Agregar Proyecto"/>
+                </form> 
+            )
+            : null
+            }
         </Fragment>
      );
 }
